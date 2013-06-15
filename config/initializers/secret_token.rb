19131c333cb4a44292c5fe8d406fbcc8c650667a
yabuki-ryosuke <yabuki-ryosuke@kayac.com>
2013-06-15 13:17:22 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OhIyeah::Application.config.secret_key_base = 'f4f325f6aa7689eceabb29b39188128739d2d3625598bd3991037f6bdd9bbe74ebb13aa26a094c4f94f1874aa7549064fc428cfa22da2811cffa13874c3adf82'
