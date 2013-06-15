bab0abbb5a5f515cb24bd930bd3286d182a3fc0c
Konboi <ryosuke.yabuki@gmail.com>
2013-06-15 19:41:33 +0900

    Fixed

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module OhIyeah
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.logger = nil
-    config.assets.enabled = false
+    #config.assets.enabled = false
 
     # AutoLoad
     config.autoload_paths += %W(#{config.root}/lib)
